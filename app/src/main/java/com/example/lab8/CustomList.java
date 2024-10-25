2c008da6ad5ed9c1ecefb0691bb694fc144540d7
AwaisAliShahid <aali786181@gmail.com>
2024-10-24 22:51:19 -0600

    implemented deleteCity in CustomList.

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -57,7 +57,7 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
     public void deleteCity(City city) {
-        // Initial empty implementation
+        cities.remove(city);
     }
 
 
