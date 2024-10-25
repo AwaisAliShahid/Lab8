b4cc0fddfa3a6221f1b433b4257a461df9603b20
AwaisAliShahid <aali786181@gmail.com>
2024-10-24 18:52:26 -0600

    upgraded the addcity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,20 +11,25 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
     public CustomList MockCityList(){
-        CustomList list= new CustomList(null, new ArrayList<>());
+        list = new CustomList(null, new ArrayList<>());
         return list;
     }
     @Test
     public void addCityTest(){
-        CustomList list = MockCityList();
-        int expectedCountBeforeAdd =0;
-        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count show");
-        City cityToAdd= new City("Edmonton", "AB");
-        list.add(cityToAdd);
-
-        int expectedCountedAfterAdd = 1;
-        assertEquals(expectedCountedAfterAdd, list.getCount(), "The City");
+//        list = MockCityList();
+//        int expectedCountBeforeAdd =0;
+//        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count show");
+//        City cityToAdd= new City("Edmonton", "AB");
+//        list.add(cityToAdd);
+//
+//        int expectedCountedAfterAdd = 1;
+//        assertEquals(expectedCountedAfterAdd, list.getCount(), "The City");
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
     }
 
 
